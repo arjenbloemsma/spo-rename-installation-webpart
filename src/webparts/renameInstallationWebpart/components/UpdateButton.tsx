3440db97078c015040815b05adcda6fc7363c4cb
Arjen Bloemsma <a.bloemsma@gmail.com>
2018-07-20 03:35:00 +0200

    Dialog function works. Next step is adding correct content

--- src/webparts/renameInstallationWebpart/components/UpdateButton.tsx
+++ src/webparts/renameInstallationWebpart/components/UpdateButton.tsx
@@ -1,62 +1,81 @@
 import * as React from 'react';
+import { bindActionCreators } from 'redux';
 import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
 import { Dialog, DialogBase, DialogType, DialogFooter } from 'office-ui-fabric-react/lib/Dialog';
 import { connect } from 'react-redux';
 import { updateSites } from '../actions';
 
-const UpdateButton = ({ dispatch, sites }) => {
-  const numberOfSitesToRename = sites.filter(s => s.siteInfo.Exists).length;
+interface Props {
+  sites: any[],
+  updateSites: any
+};
 
-  //let dialog: DialogBase | undefined;
-  let dialog: any;
-  const showDialog = (): void => {
-    dialog.setState({ hideDialog: false });
-  };
-  const closeDialog = (): void => {
-    dialog.setState({ hideDialog: true });
-  };
-  //this.state.hideDialog = true;
-  const renameInstallationSites = e => {
+interface State {
+  hideDialog: boolean
+};
+
+class UpdateButton extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = { hideDialog: true };
+  }
+  getNumberOfSitesToRename() {
+    const { sites } = this.props;
+    const numberOfSitesToRename = sites.filter(s => s.siteInfo.Exists).length;
+    return numberOfSitesToRename;
+  }
+  toggleDialog() {
+    this.setState({ hideDialog: !this.state.hideDialog })
+  }
+  renameInstallationSites(e) {
     e.preventDefault();
+    const { updateSites, sites } = this.props;
+    const numberOfSitesToRename = this.getNumberOfSitesToRename();
     if (numberOfSitesToRename <= 0) {
       return;
     }
-    dispatch(updateSites(sites));
-    showDialog();
-  };
+    updateSites(sites);
+    this.toggleDialog();
+  }
+  render() {
+    const numberOfSitesToRename = this.getNumberOfSitesToRename();
+    return (
+      <div>
+        <DefaultButton
+          primary={true}
+          onClick={() => this.toggleDialog()}
+          disabled={numberOfSitesToRename <= 0}>
+          Update {numberOfSitesToRename} Installation site(s)
+        </DefaultButton>
+        <Dialog
+          hidden={this.state.hideDialog}
+          onDismiss={() => this.toggleDialog()}
+          dialogContentProps={{
+            type: DialogType.normal,
+            title: 'Validate updated sites',
+            subText:
+            'Validate the following updated Installation sites:'
+          }}
+          modalProps={{
+            titleAriaId: 'myLabelId',
+            subtitleAriaId: 'mySubTextId',
+            isBlocking: false,
+            containerClassName: 'ms-dialogMainOverride'
+          }}
+        >
+          {null /** You can also include null values as the result of conditionals */}
+          <DialogFooter>
+            <PrimaryButton onClick={(e) => this.renameInstallationSites(e)} text="Save" />
+            <DefaultButton onClick={() => this.toggleDialog()} text="Cancel" />
+          </DialogFooter>
+        </Dialog>
+      </div>);
+  }
+}
 
-  return (
-    <div>
-      <DefaultButton
-        primary={true}
-        onClick={renameInstallationSites}
-        disabled={numberOfSitesToRename <= 0}>
-        Update {numberOfSitesToRename} Installation site(s)
-      </DefaultButton>
-      <Dialog
-        ref={d => (dialog = d!)}
-        hidden={true}
-        onDismiss={closeDialog}
-        dialogContentProps={{
-          type: DialogType.normal,
-          title: 'Validate updated sites',
-          subText:
-          'Validate the following updated Installation sites:'
-        }}
-        modalProps={{
-          titleAriaId: 'myLabelId',
-          subtitleAriaId: 'mySubTextId',
-          isBlocking: false,
-          containerClassName: 'ms-dialogMainOverride'
-        }}
-      >
-        {null /** You can also include null values as the result of conditionals */}
-        <DialogFooter>
-          <PrimaryButton onClick={closeDialog} text="Save" />
-          <DefaultButton onClick={closeDialog} text="Cancel" />
-        </DialogFooter>
-      </Dialog>
-    </div>);
-};
-
-export default connect()(UpdateButton);
+export default connect(
+  state => ({ sites: state.sites }),
+  dispatch => bindActionCreators({
+    updateSites
+  }, dispatch),
+)(UpdateButton);
